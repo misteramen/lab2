956af0ae707a4f75ba6c6f0f2bd91acfe7ab3b9c
misteramen <ndi15jle@student.hig.se>
2016-10-13 11:47:10 +0200

    Update FigureController.java

--- ctrl/FigureController.java
+++ ctrl/FigureController.java
@@ -24,7 +24,7 @@ import org.laboration.two.model.Vertex2D;
  * @author Andreas Brodin
  * @author Niklas Lindfors
  * @author Joshua Lutakome Yawe
- * @version 2.0
+ * @version 1.0
  *
  */
 // HEHEHEJEJHJ
