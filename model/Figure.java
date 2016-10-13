36ce95785e39cbe8668271b8ad797e2e1effef05
misteramen <ndi15jle@student.hig.se>
2016-10-13 17:46:23 +0200

    Removed the translate() method.
    I removed the translate() method, which will be added to a new interface called Movable. There's been such a push -- by both Peter and Jackson -- towards adding this interface separately to the classes that need it, that I in this branch attempt at making that change.

--- model/Figure.java
+++ model/Figure.java
@@ -12,16 +12,6 @@ package org.laboration.two.model;
  */
 public abstract class Figure {
 	/**
-	 * This method moves a figure in the x-axis by a distance of <code>dx</code> and
-	 * in the y-axis by a distance of <code>dy</code>, from the figures original
-	 * location.
-	 * 
-	 * @param dx - The distance moved in the x-axis
-	 * @param dy - The distance moved in the y-axis
-	 * @return Figure - A new translated instance of the <code>Figure</code> class is returned.
-	 */
-	public abstract Figure translate(double dx, double dy);
-	/**
 	 * This method returns the center coordinate of the figure.
 	 * 
 	 * @return Vertex2D - The center coordinate of the figure.
