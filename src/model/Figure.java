f856578c166dbdb9ba04fbe2985e47da09052fb6
misteramen <ndi15jle@student.hig.se>
2016-10-18 10:35:48 +0200

    Update Figure.java
    In the recent merge I forgot to put back the translate method.

--- src/model/Figure.java
+++ src/model/Figure.java
@@ -17,4 +17,14 @@ public abstract class Figure {
 	 * @return Vertex2D - The center coordinate of the figure.
 	 */
 	public abstract Vertex2D center();
+	/**
+	 * This method moves a figure in the x-axis by a distance of <code>dx</code> and
+	 * in the y-axis by a distance of <code>dy</code>, from the figures original
+	 * location.
+	 * 
+	 * @param dx - The distance moved in the x-axis
+	 * @param dy - The distance moved in the y-axis
+	 * @return Figure - A new translated instance of the <code>Figure</code> class is returned.
+	 */
+	public abstract Figure translate(double dx, double dy);
 }
